ede2409ec0045e63be680a30baecdf675bccc3d8
Chase <burgesscharles89@gmail.com>
2022-03-27 20:57:11 -0400

    initial project

---
+++ Entities/AutoMapper/AutomapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Model;
+using Entities.DTO;
+using AutoMapper;
+
+namespace Entities.AutoMapper
+{
+    public class AutomapperProfile: Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Employee, Employee_DTO>().ReverseMap();
+        }
+    }
+}
---
+++ Entities/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Entities.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Department { get; set; }
+        public DateTime? HireDate { get; set; }
+    }
+}
